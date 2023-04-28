419c208b95542d834861126236b657feb2bb63b6
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-04-27 20:30:42 -0400

    ss

--- script.js
+++ script.js
@@ -17,23 +17,22 @@ $(".mobile-toggle").on("click", function () {
   }
 });
 */
-$(document).ready(function () {
-  $(".mobile-toggle").click(function () {
-    $(this).toggleClass("active");
-    $(".main-header").toggleClass("open-nav");
-  });
 
-  // navigation scroll lijepo radi materem
-  $("nav a").on("click", function (event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $("html, body").animate(
-      {
-        scrollTop: target,
-      },
-      500
-    );
-    evt.preventDefault();
-  });
+$(".mobile-toggle").click(function () {
+  $(this).toggleClass("active");
+  $(".main-header").toggleClass("open-nav");
+});
+
+// navigation scroll lijepo radi materem
+$("nav a").on("click", function (event) {
+  var id = $(this).attr("href");
+  var offset = 70;
+  var target = $(id).offset().top - offset;
+  $("html, body").animate(
+    {
+      scrollTop: target,
+    },
+    500
+  );
+  evt.preventDefault();
 });
